9238ca4572ad22d694685edfb19108ea97072617
R3xix <piirsoo.reimo@gmail.com>
2024-10-14 16:58:15 +0300

    react algus

---
+++ veebipood/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+
+// igal HTML elemendil peab olema algus ja lõpp või ta on self-closing
+// HTMLi omadused pannakse alguse sisse: <div OMADUSED_SIIA></div>
+
+function App() {
+  return (
+    <div className="App">
+     <button className="nupp">Nupp</button>
+     <img className="pilt" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fsupercarspro.files.wordpress.com%2F2016%2F05%2Fsports-cars-1.jpg&f=1&nofb=1&ipt=19af6d8b1d1d3a347d8114a06a8613d794f269ff611fa9ee423a857756757a0f&ipo=images" alt="" />
+    </div>
+  );
+}
+
+export default App;
